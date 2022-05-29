6e0681fa145dd8e8fe2610eb488faf72691aeb13
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-29 12:48:36 +0300

    music Card created

---
+++ src/components/MusicCard/Music.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import {View,Text} from 'react-native';
+
+function App() {
+    return(
+        <View>
+        <Text> Deneme </Text>
+        </View>
+    )
+};
+
+export default App;
