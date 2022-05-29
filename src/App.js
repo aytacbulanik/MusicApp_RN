0a64090c03be8da02debcd010c501d60b82bb569
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-29 12:52:09 +0300

    cards name was changed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import MusicCard from './components/MusicCard';
 
 const App = () => {
