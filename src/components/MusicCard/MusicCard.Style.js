cdc80b53270ab7b4c9a6aad4de256d606ea5398b
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-31 22:57:18 +0300

    flatlist design added

--- src/components/MusicCard/MusicCard.Style.js
+++ src/components/MusicCard/MusicCard.Style.js
@@ -5,15 +5,30 @@ export default StyleSheet.create({
         flex: 1,
         backgroundColor : 'lightgray',
         flexDirection: 'row',
-        borderWidth: 1,
     },
     textContainer: {
-
+        flex:1,
+        margin: 3,
+        padding: 2,
+        borderWidth: 1,
+    },
+    topTextView: {
+        flex: 2,
+        flexDirection: 'row',
+    },
+    topText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    bottomTextView : {
+        flex:2,
+        flexDirection: 'row',
     },
     image : {
         width: Dimensions.get('screen').width /4,
         height:100,
         borderRadius: 50,
+        margin: 3,
     },
 
 })
--- src/components/MusicCard/MusicCard.js
+++ src/components/MusicCard/MusicCard.js
@@ -3,15 +3,22 @@ import React from 'react';
 import {View,Text, Image} from 'react-native';
 import cardStyle from './MusicCard.Style';
 
-const App = ({music}) => {
-    return(
-        <View style={cardStyle.container}>
+const App = ({music}) =>  {
+        return (<View style={cardStyle.container}>
             <Image style={cardStyle.image}
-            source={{uri: music.imageUrl}}
-            />
-        <Text> {music.title} </Text>
-        </View>
-    )
-};
+                source={{ uri: music.imageUrl }} />
+            <View style={cardStyle.textContainer}>
+                <View style={cardStyle.topTextView}>
+                    <Text style={cardStyle.topText}> {music.title} </Text>
+                    <Text> {music.artist} </Text>
+                </View>
+                <View style={cardStyle.bottomTextView}>
+                    <Text> {music.album} </Text>
+                    <Text> {music.year} </Text>
+                </View>
+            </View>
+        </View>)
+    }
+
 
 export default App;
